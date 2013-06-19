f04a525a1c036e9eaae841ea696cb4294ba4cc46
Chris Sanders <astericky@gmail.com>
2013-06-19 15:03:48 -0400

    Update Test Framework

--- The-Open-Closed-Principle/java/src/test/java/com/theladders/solid/ocp/TestIt.java
+++ The-Open-Closed-Principle/java/src/test/java/com/theladders/solid/ocp/TestIt.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import com.theladders.solid.ocp.jobseeker.JobseekerConfidentialityProfileDao;
 import com.theladders.solid.ocp.resume.ConfidentialResumeHandler;
 import com.theladders.solid.ocp.resume.JobseekerProfileManager;
+import com.theladders.solid.ocp.resume.Phrase;
 import com.theladders.solid.ocp.resume.ResumeConfidentialityManager;
 import com.theladders.solid.ocp.user.User;
 
@@ -19,14 +20,22 @@ public class TestIt
 
   int id = 1; // get from command line?
   User user = new User(id);
+  int count = 0;
   
   @Test
   public void makeAllCategoriesConfidential() {
-    //resumeConfidentialityManager.makeAllCategoriesNonConfidential(user);
-    System.out.println("hello world");
-    assertEquals(true, true);
+    String[] phrases = {"Name", "MailingAddress", "PhoneNumber", "EmailAddress", "ContactInfo", "CompanyName", "WorkExperience"};
+    boolean isConfidential = false;
+    resumeConfidentialityManager.makeAllCategoriesNonConfidential(user);
+
+    for(String phrase : phrases){
+      Phrase newPhrase = new Phrase(phrase);
+      assertTrue(newPhrase.isConfidential());
+    }
+    
   }
   
+  @Test
   public void makeAllCategoriesNonConfidential() {
     resumeConfidentialityManager.makeAllContactInfoNonConfidential(user);
     assertEquals(true, true);
